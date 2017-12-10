5dac45cb95dc03223696cee2586b149064be1384
Zachary Lentz <zlentz@slac.stanford.edu>
2017-12-10 09:32:19 -0800

    ENH: Add experiment file loader

---
+++ hutch_python/yaml_experiment.py
@@ -0,0 +1,14 @@
+import utils
+
+
+def load_objs(info):
+    """
+    Load the experiments/expname file for experiment-specific includes
+    """
+    objs = []
+    modules = utils.interpret_list(info)
+    experiments = ['experiments.' + m for m in modules]
+    for experiment in experiments:
+        module_objs = utils.extract_objs(experiment)
+        objs.extend(module_objs)
+    return objs
