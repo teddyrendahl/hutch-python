0878b6d2b8ab230180364c3e195002bd6aeb3fac
Zachary Lentz <zlentz@slac.stanford.edu>
2017-12-13 17:32:29 -0500

    ENH: Add options for expname.object and expname.class()

--- hutch_python/utils.py
+++ hutch_python/utils.py
@@ -41,10 +41,16 @@ def extract_objs(module_name):
     Import module and return all the objects without a _ prefix. If an __all__
     keyword exists, follow that keyword's instructions instead.
 
+    If this is a single object in a module rather than a module, import just
+    that object.
+
+    If this is a callable and it ends in (), call it and import the return
+    value. Note that this includes classes.
+
     Parameters
     ----------
     module_name: str
-        Filename or module name
+        Filename, module name, or path to object in module
 
     Returns
     -------
@@ -54,8 +60,23 @@ def extract_objs(module_name):
     objs = {}
     # Allow filenames
     module_name = module_name.strip('.py')
+    if '()' in module_name:
+        module_name.strip('()')
+        call_me = True
+    else:
+        call_me = False
     try:
-        module = importlib.import_module(module_name)
+        try:
+            module = importlib.import_module(module_name)
+        except ImportError:
+            my_obj = find_object(module_name)
+            name = module_name.split('.')[-1]
+            # call_me, maybe
+            if call_me:
+                objs[name] = my_obj()
+            else:
+                objs[name] = my_obj
+            return objs
     except Exception:
         logger.exception('Error loading %s', module_name)
         return objs
@@ -108,6 +129,27 @@ def assign_name(obj):
     return name
 
 
+def find_object(obj_path):
+    """
+    Given a string module path to an object, return that object.
+
+    Parameters
+    ----------
+    obj_path: str
+        String module path to an object
+
+    Returns
+    -------
+    obj: Object
+        That object
+    """
+    parts = obj_path.split('.')
+    module_path = '.'.join(parts[:-1])
+    class_name = parts[-1]
+    module = importlib.import_module(module_path)
+    return getattr(module, class_name)
+
+
 def find_class(class_path):
     """
     Given a string class name, either return the matching built-in type or
@@ -123,10 +165,6 @@ def find_class(class_path):
     cls: type
     """
     if '.' in class_path:
-        parts = class_path.split('.')
-        module_path = '.'.join(parts[:-1])
-        class_name = parts[-1]
-        module = importlib.import_module(module_path)
-        return getattr(module, class_name)
+        return find_object(class_path)
     else:
         return eval(class_path)
