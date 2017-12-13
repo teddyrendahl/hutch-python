24f2e6d000a929098b71091199a150270d445a3b
Zachary Lentz <zlentz@slac.stanford.edu>
2017-12-13 15:10:47 -0500

    ENH: Move most of the cli startup into the module layer

---
+++ hutch_python/cli.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import argparse
+
+from .load_conf import load
+from .log_setup import setup_logging
+
+
+def setup_cli_env():
+    # Parse the user's arguments
+    parser = argparse.ArgumentParser(description='Launch LCLS Hutch Python')
+    parser.add_argument('--cfg', required=True,
+                        help='Configuration yaml file')
+    args = parser.parse_args()
+
+    # Make sure the hutch's directory is in the path
+    sys.path.insert(0, os.getcwd())
+
+    # Set up logging
+    setup_logging()
+
+    # Load objects from the configuration file
+    return load(args.cfg)
