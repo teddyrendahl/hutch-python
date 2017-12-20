e1f4894fc7159ba5418bc375a1a16eb5f9dba857
Zachary Lentz <zlentz@slac.stanford.edu>
2017-12-20 15:53:56 -0800

    TST: commit to deprecating list objs

--- hutch_python/tests/plugins/test_namespace.py
+++ hutch_python/tests/plugins/test_namespace.py
@@ -8,16 +8,14 @@ logger = logging.getLogger(__name__)
 
 def test_namespace_plugin_class():
     logger.debug('test_namespace_plugin_class')
-    objs = {'catagory': {'one': 1, 'two': 2.0, 'three': '3'},
-            'my_list': ['apples', 4]}
+    objs = {'one': 1, 'two': 2.0, 'three': '3'}
     info = {'class': {'float': ['flt'],
                       'str': ['text', 'words']}}
     plugin = Plugin(info)
     namespaces = plugin.get_objects()
-    plugin.future_plugin_hook('something', objs)
+    plugin.future_plugin_hook(None, objs)
     float_space = namespaces['flt']
     assert float_space.two == 2.0
     string_space = namespaces['text']
     assert string_space.three == '3'
-    assert string_space.str == 'apples'
     assert string_space == namespaces['words']
