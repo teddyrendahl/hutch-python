44bdc9d654c0859aea3260f18fd9f61a879a85c2
Teddy Rendahl <trendahl@slac.stanford.edu>
2018-01-17 10:45:15 -0800

    STY: E302 expected 2 blank lines, found 1

--- hutch_python/tests/test_plugins/test_happi.py
+++ hutch_python/tests/test_plugins/test_happi.py
@@ -5,6 +5,7 @@ from hutch_python.plugins.happi import Plugin
 
 logger = logging.getLogger(__name__)
 
+
 def test_happi_plugin():
     _db = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                        '../happi_db.json')
