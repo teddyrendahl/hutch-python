7401dea1e279046c4f927c2a76e4ef3a97f6bb01
Teddy Rendahl <trendahl@slac.stanford.edu>
2018-01-30 14:19:26 -0800

    MAINT: Move mock QSBackend to conftest

--- hutch_python/tests/test_plugins/test_questionnaire.py
+++ hutch_python/tests/test_plugins/test_questionnaire.py
@@ -5,33 +5,9 @@ import pytest
 import hutch_python.plugins.questionnaire
 from hutch_python.plugins.questionnaire import Plugin
 
+from ..conftest import QSBackend
 
 logger = logging.getLogger(__name__)
-
-
-class QSBackend:
-    def __init__(self, run, proposal):
-        self.run = run
-        self.proposal = proposal
-
-    def find(self, multiples=False, **kwargs):
-        devices = [{
-            '_id': 'TST:USR:MMN:01',
-            'beamline': 'TST',
-            'device_class': 'hutch_python.tests.conftest.Experiment',
-            'location': 'Hutch-main experimental',
-            'args': ['{{run}}', '{{proposal}}'],
-            'kwargs': {},
-            'name': 'inj_x',
-            'prefix': 'TST:USR:MMN:01',
-            'purpose': 'Injector X',
-            'type': 'Device',
-            'run': self.run,
-            'proposal': self.proposal}]
-        if multiples:
-            return devices
-        else:
-            return devices[0]
 
 
 def test_questionnaire_plugin():
