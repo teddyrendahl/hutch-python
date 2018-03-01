a1541ee1a4f34119b68a726e29d594ff6231727f
teddyrendahl <trendahl@slac.stanford.edu>
2018-03-01 10:13:30 -0800

    TST: Add a test for get_lightpath

--- hutch_python/tests/test_happi.py
+++ hutch_python/tests/test_happi.py
@@ -1,7 +1,7 @@
 import os.path
 import logging
 
-from hutch_python.happi import get_happi_objs
+from hutch_python.happi import get_happi_objs, get_lightpath
 
 logger = logging.getLogger(__name__)
 
@@ -14,3 +14,13 @@ def test_happi_objs():
     objs = get_happi_objs(db, 'tst')
     assert len(objs) == 2
     assert all([obj.active for obj in objs.values()])
+
+
+def test_get_lightpath():
+    logger.debug("test_get_lightpath")
+    db = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                      'happi_db.json')
+    obj = get_lightpath(db, 'tst')
+    # Check that we created a valid BeamPath with no inactive objects
+    assert obj.name == 'TST'
+    assert len(obj.devices) == 2
