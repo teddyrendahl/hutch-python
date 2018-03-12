a37d7aa79863a5051ba58a021dac36e2b0b1e454
Zachary Lentz <zlentz@slac.stanford.edu>
2018-03-12 14:01:03 -0700

    ENH: Add platforms for current hutches

--- hutch_python/constants.py
+++ hutch_python/constants.py
@@ -4,7 +4,13 @@ CUR_EXP_SCRIPT = '/reg/g/pcds/engineering_tools/{0}/scripts/get_curr_exp {0}'
 
 CLASS_SEARCH_PATH = ['pcdsdevices.device_types']
 
-DAQ_MAP = dict(mfx=4,
+DAQ_MAP = dict(amo=0,
+               sxr=0,
+               xpp=1,
+               xcs=1,
+               mfx=4,
+               cxi=4,
+               mec=0,
                tst=0)
 
 DIR_MODULE = Path(__file__).resolve().parent
