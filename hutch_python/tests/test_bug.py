006724c0799c859d5aacdd1ee80d7b1662757a84
teddyrendahl <trendahl@slac.stanford.edu>
2018-04-03 16:25:00 -0700

    TST: Combine bug_report post_to_github tests

--- hutch_python/tests/test_bug.py
+++ hutch_python/tests/test_bug.py
@@ -6,8 +6,8 @@ import simplejson
 from requests import Response
 
 import hutch_python.bug
-from hutch_python.bug import (get_current_environment, post_to_github,
-                              report_bug, get_text_from_editor)
+from hutch_python.bug import (get_current_environment, report_bug,
+                              get_text_from_editor)
 
 logger = logging.getLogger(__name__)
 
@@ -19,11 +19,27 @@ def user_input(prompt):
 
 # Mock requests.Session to avoid actual GitHub posts
 class FakeSession:
+    handle = None
+    past_auth = dict()
 
     def __init__(self):
-        self.auth = None
+        pass
+
+    @property
+    def auth(self):
+        return self.past_auth
+
+    @auth.setter
+    def auth(self, auth_info):
+        logger.debug('Updating authorization %s', auth_info)
+        self.past_auth.update(dict([auth_info]))
 
     def post(self, url, json_dump):
+        # Dump the JSON to handle
+        if self.handle:
+            logger.debug('Dumping JSON to %s', self.handle.name)
+            self.handle.write(json_dump)
+        # Create a fake response
         r = Response()
         r.status_code = 201
         return r
@@ -46,37 +62,24 @@ def test_get_current_environment():
         assert env == 'test-environment'
 
 
-def test_bug_report(monkeypatch):
+def test_bug_report(monkeypatch, temporary_config):
     logger.debug('test_bug_report')
     # Patch in our fake user input function
     monkeypatch.setattr(hutch_python.bug, 'request_input', user_input)
-    # Set a fake environment name
-    os.environ['CONDA_ENVNAME'] = 'test-environment'
-    with tempfile.TemporaryDirectory() as tmp:
-        # Make sure we write to this directory
-        hutch_python.bug.BUG_REPORT_PATH = tmp
-        # Gather report
-        bug_path = report_bug(captured_output='A printed message',
-                              description='A description of the bug')
-        bug = simplejson.load(open(bug_path, 'r'))
-    # See that we catch the fake environment
-    assert bug['env'] == 'test-environment'
-    # See that bug reporting captures all keys
-    bug_keys = ['author', 'commands', 'description', 'env', 'logfiles',
-                'title', 'output', 'dev_pkgs']
-    assert all([key in bug for key in bug_keys])
-
-
-def test_post_to_github(monkeypatch):
-    logger.debug('test_post_to_github')
-    # Create a fake issue and requests.Session
+    # Patch in fake requests.Session
     monkeypatch.setattr(hutch_python.bug.requests, 'Session', FakeSession)
-    fake_post = {'title': 'This is a Test Issue'}
+    # Create a fake issue and requests.Session
     with tempfile.NamedTemporaryFile('w+', delete=False) as tmp:
-        simplejson.dump(fake_post, open(tmp.name, 'w+'))
-        post_to_github(tmp.name, 'user', pw='pw')
-        fname = tmp.name
-    assert not os.path.exists(fname)
+        # Write to our fake file instead of GitHub
+        FakeSession.handle = tmp
+        # Post report
+        report_bug(captured_output='A printed message',
+                   description='A description of the bug')
+    bug = simplejson.load(open(tmp.name, 'r'))
+    # Check GitHub authentication
+    assert FakeSession.past_auth == {'github_user': 'github_pw'}
+    # See that we catch the fake environment
+    assert bug['title'].startswith('Please')
 
 
 def test_get_text_from_editor(monkeypatch):
