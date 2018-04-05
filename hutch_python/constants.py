59562749879db12f05594379e1a003d55b518542
Zachary Lentz <zlentz@slac.stanford.edu>
2018-04-05 11:04:35 -0700

    DEP: remove DAQ_MAP

--- hutch_python/constants.py
+++ hutch_python/constants.py
@@ -4,15 +4,6 @@ CUR_EXP_SCRIPT = '/reg/g/pcds/engineering_tools/{0}/scripts/get_curr_exp {0}'
 
 CLASS_SEARCH_PATH = ['pcdsdevices.device_types']
 
-DAQ_MAP = dict(amo=0,
-               sxr=0,
-               xpp=1,
-               xcs=1,
-               mfx=4,
-               cxi=4,
-               mec=0,
-               tst=0)
-
 DIR_MODULE = Path(__file__).resolve().parent
 
 FILE_YAML = DIR_MODULE / 'logging.yml'
@@ -30,4 +21,4 @@ INPUT_LEVEL = 5
 
 SUCCESS_LEVEL = 35
 
-VALID_KEYS = ('hutch', 'db', 'load', 'experiment')
+VALID_KEYS = ('hutch', 'db', 'load', 'experiment', 'daq_platform')
